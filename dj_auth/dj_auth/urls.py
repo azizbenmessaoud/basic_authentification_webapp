a16289156d9ed49341ba06099752fee431d4d993
[azizbenmessaoud] <[bnmessaoudaziz@gmail.com]>
2022-11-15 05:45:03 +0200

    configure Oauth urls

--- dj_auth/dj_auth/urls.py
+++ dj_auth/dj_auth/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    #...
+    path('', TemplateView.as_view(template_name="home.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
     path('admin/', admin.site.urls),
 ]
